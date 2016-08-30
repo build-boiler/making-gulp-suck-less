62bbdc3c4d96d4083011aae03736cd38f6f0d0de
dtothefp <dtothefp@gmail.com>
2016-08-30 00:23:36 -0400

    add fallback build

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,42 +1,38 @@
 import 'babel-polyfill';
-import path from 'path';
 import gulp from 'gulp';
-import load from 'gulp-load-plugins';
-import {sync as globSync} from 'globby';
 import babel from './tasks/babel';
 
 const {TRAVIS_BRANCH} = process.env;
 const isCi = !!TRAVIS_BRANCH;
+const opts = {
+  plugins: {
+    //config,
+    //rename,
+    //lazy,
+    //patterns
+  },
+  config: {
+    environment: {
+      isCi
+    }
+    //sources,
+    //tasks,
+    //utils
+  },
+  dirs: {
+    tasks: [
+      'tasks',
+      'packages/gulpy-boiler-task-ava',
+      'packages/gulpy-boiler-task-eslint'
+    ]
+  },
+  wrapper: {
+    //args
+  }
+};
 
 try {
   const bootstrap = require('./packages/gulpy-boiler-core/src');
-  const opts = {
-    plugins: {
-      //config,
-      //rename,
-      //lazy,
-      //patterns
-    },
-    config: {
-      environment: {
-        isCi
-      }
-      //sources,
-      //tasks,
-      //utils
-    },
-    dirs: {
-      tasks: [
-        'tasks',
-        'packages/gulpy-boiler-task-ava',
-        'packages/gulpy-boiler-task-eslint'
-      ]
-    },
-    wrapper: {
-      //args
-    }
-  };
-
   const {tasks, plugins, config} = bootstrap(gulp, opts);
   const {utils} = config;
   const {addbase} = utils;
@@ -51,8 +47,6 @@ try {
   gulp.task('babel', babel(gulp, plugins, config));
   gulp.task('assemble', tasks.assemble);
 
-  const baseTasks = gulp.series('lint', 'babel');
-
   gulp.task('watch:build', () => {
     gulp.watch([
       addbase('tasks/**/*.js'),
@@ -65,71 +59,13 @@ try {
     ]).on('change', gulp.series('lint:test'));
   });
 
-  gulp.task('build', gulp.series(baseTasks, 'webpack', 'assemble'));
+  gulp.task('build', gulp.series('lint', 'babel', 'webpack', 'assemble'));
   gulp.task('watch', gulp.series('build', 'watch:build'));
   gulp.task('default', gulp.series('babel'));
 } catch (err) {
   if (!isCi) {
-    console.log('**FALLING BACK TO DEFAULT BUILD', err.message, err.stack);
+    console.error('**FALLING BACK TO DEFAULT BUILD**', err.message, err.stack);
   }
 
-  const loadOpts = {
-    pattern: [
-      '!gulpy',
-      'gulp-*',
-      'gulp.*',
-      'del',
-      'run-sequence',
-      'browser-sync'
-    ],
-    lazy: false,
-    rename: {
-      'gulp-util': 'gutil',
-      'run-sequence': 'sequence',
-      'gulp-if': 'gulpIf'
-    }
-  };
-
-  const packages = ['./package.json', ...globSync('./packages/*/package.json')];
-  const plugins = packages.reduce((acc, fp) => {
-    const config = path.resolve(fp);
-    const opts = Object.assign({}, loadOpts, {config});
-    let plugins;
-
-    try {
-      plugins = load(opts);
-    } catch (err) {
-      // eslint-disable-line
-    }
-
-    return {
-      ...acc,
-      ...plugins
-    };
-  }, {});
-  const isDev = process.argv.includes('watch');
-  const environment = {
-    isLocalDev: true,
-    isDev
-  };
-
-  const utils = {
-    addbase(...args) {
-      return path.join.apply(path, [process.cwd(), ...args]);
-    },
-    addroot(...args) {
-      return this.addbase.apply(this, ['packages', ...args]);
-    },
-    getTaskName({name}) {
-      return name.split(':').slice(-1)[0];
-    }
-  };
-
-  const sources = {
-    buildDir: 'dist'
-  };
-
-  const config = {environment, sources, utils};
-
-  gulp.task('babel', babel(gulp, plugins, config));
+  require('./fallback');
 }
