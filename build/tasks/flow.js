0f63e28446324209bf86d4546071f9a33d7c410c
dtothefp <dtothefp@gmail.com>
2016-09-07 17:00:10 -0400

    try to publish on travis

--- build/tasks/flow.js
+++ build/tasks/flow.js
@@ -3,8 +3,9 @@ import {TaskHandler} from '../../packages/gulpy-boiler-utils'
 
 export default class Flow extends TaskHandler {
   task(gulp, plugins, config) {
-    const {utils} = config
+    const {environment, utils} = config
     const {addbase} = utils
+    const {isCi} = environment
     const {flowtype} = plugins
     const src = [
       addbase('build', '**/*.js'),
@@ -14,8 +15,8 @@ export default class Flow extends TaskHandler {
       all: false,
       weak: false,
       declarations: './declarations/',
-      killFlow: false,
-      beep: true,
+      killFlow: isCi,
+      beep: !isCi,
       abort: false,
       reporter
     }
